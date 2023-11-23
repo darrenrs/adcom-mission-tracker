c5d2678f41a5a3a0de85b6250ca27b05dbf69167
darrenrs <darrenrs0515@gmail.com>
2023-11-23 15:23:46 -0700

    Updated Pet Show and Ages schedule

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ var BALANCE_UPDATE_VERSION = {
   "crusade-bal-1": "6.14",
   "defense-bal-21": "6.14",
   "export-bal-21": "6.29",
-  "fusfarm-bal-30": "6.25",
+  "fusfarm-bal-30": "6.30",
   "fuspet-bal-32": "6.30",
   "fusscience-bal-31": "6.30",
   "fusvehicle-bal-32": "6.18",
